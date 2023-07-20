42eaf2227a8a959923ae35cdc852532bd0526d6c
LazezBZH <lazez.bzh@caramail.fr>
2023-07-20 22:29:41 +0200

    maj 20 07 20

--- script.js
+++ script.js
@@ -1,73 +1,199 @@
 const category = document.getElementById("category");
-const category0 = document.getElementById("category0");
+const categoryForAdd = document.getElementById("categoryForAdd");
+
 const products = document.getElementById("products");
+const productsLabel = document.getElementById("productsLabel");
+const quantity = document.getElementById("quantity");
+const unity = document.getElementById("unity");
 const add = document.getElementById("add");
+const resultUl = document.getElementById("result");
+const resultList = document.getElementById("list");
+
+const getCat = document.getElementById("getCat");
+const catToAdd = document.getElementById("catToAdd");
 
 let categoryName = "";
 let productName = "";
+let monId = "";
+let quantityName = "";
+let unityName = "";
 
-let fruits = [];
-let vegetable = [];
-let salt = [];
-let sugar = [];
-let milk = [];
+let list = [];
+let listSet = [];
 
-category.addEventListener("change", getCategory);
+category.addEventListener("click", getCategory);
 products.addEventListener("click", getProduct);
+quantity.addEventListener("change", getQuantity);
+unity.addEventListener("click", getUnity);
 add.addEventListener("click", addProduct);
+getCat.addEventListener("click", getTheCat);
 
-function addProduct() {}
+// CATEGORIES
 
-function erase(e) {
-  e.preventdefault();
-  products.style.opacity = "0";
+let dataCat = JSON.parse(localStorage.getItem("categories")) || [];
+let dataProd = JSON.parse(localStorage.getItem("products")) || [];
+// category.innerHTML = `<option value="0 >choisissez</option>`;
+if (!localStorage.getItem("categories")) {
+  fetch("/data/categories.json")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      dataCat = JSON.stringify(data);
+
+      localStorage.setItem("categories", dataCat);
+
+      data.forEach(function (element) {
+        (category.innerHTML += `<option value=${element.value} >${element.name}</option>`),
+          (categoryForAdd.innerHTML += `<option value=${element.value} >${element.name}</option>`),
+          (resultUl.innerHTML += `<li  >${element.name}<ul  class="toSort" id= ${element.value}></ul></li>`);
+      });
+    });
+} else {
+  dataCat = JSON.parse(localStorage.getItem("categories"));
+  dataCat.forEach(function (element) {
+    (category.innerHTML += `<option value=${element.value} >${element.name}</option>`),
+      (categoryForAdd.innerHTML += `<option value=${element.value} >${element.name}</option>`),
+      (resultUl.innerHTML += `<li  >${element.name}<ul class="toSort" id= ${element.value}></ul></li>`);
+  });
 }
 
+// catégories
 function getCategory(e) {
   categoryName = e.target.value;
-  console.log(categoryName);
-  if (categoryName == "0") {
+
+  if (!categoryName) {
     products.style.opacity = "0";
+    productsLabel.style.opacity = "0";
   } else {
-    createProductsList();
     products.style.opacity = "1";
+    productsLabel.style.opacity = "1";
+    setProducts(categoryName);
   }
+
+  return categoryName;
 }
 
-function createProductsList() {
-  let productsList = [];
-  products.innerHTML = "";
+// produits
 
-  switch (categoryName) {
-    case "fruits":
-      productsList = ["Pommes", "Poires", "Bananes"];
-      break;
-    case "vegetable":
-      productsList = ["Poireaux", "Carottes", "Choux"];
-      break;
-    case "salt":
-      productsList = ["Sel", "Poivre", "Chips"];
-      break;
-    case "sugar":
-      productsList = ["Coockies", "Confiture", "Sucre"];
-      break;
-    case "milk":
-      productsList = ["Beurre", "Oeufs", "Crème"];
-      break;
-    default:
-      console.log(`oups.`);
+function setProducts() {
+  products.innerHTML = `<option value="0" >Choisissez</option>`;
+  if (!localStorage.getItem("products")) {
+    fetch("/data/products.json")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        dataProd = JSON.stringify(data);
+
+        localStorage.setItem("products", dataProd);
+
+        let prod = data.filter((dat) => dat.product == categoryName)[0];
+
+        prod.items.forEach(
+          (element) =>
+            (products.innerHTML += `<option class=${element.id} value=${element.value} >${element.name}</option>`)
+        );
+      });
   }
+  dataProd = JSON.parse(localStorage.getItem("products"));
 
-  productsList.forEach(
-    (product) =>
-      (products.innerHTML += `<option value=${product} >${product}</option>`)
+  let prod = dataProd.filter((dat) => dat.product == categoryName)[0];
+
+  prod.items.forEach(
+    (element) =>
+      (products.innerHTML += `<option class=${element.id} value=${element.value} >${element.name}</option>`)
   );
-  console.log(productsList);
-  return productsList;
 }
 
+// ajout cat
+
+function getTheCat() {
+  let val = catToAdd.value;
+  let toAdd = {
+    name: val,
+    value: val,
+  };
+  if (val.length >= 1) {
+    dataCat.push(toAdd);
+
+    dataCat = JSON.stringify(dataCat);
+    localStorage.setItem("categories", dataCat);
+    window.location.reload();
+  }
+}
+
+// obtenir le produit
 function getProduct(e) {
+  add.disabled = false;
   productName = e.target.value;
-  console.log(productName);
-  return productName;
+  let selectElement = e.target;
+  let optionSelectionnee = selectElement.selectedOptions[0];
+  let monId = optionSelectionnee.className;
+  console.log("prodname", productName, "monID", monId);
+  return (thisid = monId);
+}
+
+// Ajout produit aux listes
+function addProduct() {
+  add.disabled = true;
+  if (productName) {
+    let resultProd = "";
+    list.push({ productName, thisid });
+    list.sort();
+    let listSet = list.filter((item, index, self) => {
+      return (
+        index ===
+        self.findIndex((el) => {
+          return (
+            el.productName === item.productName && el.thisid === item.thisid
+          );
+        })
+      );
+    });
+
+    console.log("arrayunique", listSet);
+
+    console.log("thisid", thisid, "monid", thisid);
+
+    listSet = listSet.filter((listItem) => listItem.thisid === thisid);
+
+    resultProd = document.getElementById(thisid);
+    resultProd.innerHTML = "";
+    listSet.forEach(
+      (product) => (
+        console.log(thisid, resultProd, product),
+        (resultProd.innerHTML += `<li> ${product.productName} </li>`)
+      )
+    );
+  }
+  sortUl();
+}
+
+// obtenir la quantité
+function getQuantity(e) {
+  quantityName = e.target.value;
+  console.log(quantityName);
+}
+// obtenir l'unité'
+function getUnity(e) {
+  unityName = e.target.value;
+  console.log(unityName);
+  //   return productName;
+}
+
+// ordre alpha
+const ulElements = document.querySelectorAll(".toSort");
+function sortUl() {
+  ulElements.forEach(function (ul) {
+    const liElements = ul.getElementsByTagName("li");
+    let liArray = Array.from(liElements);
+    liArray.sort(function (a, b) {
+      return a.textContent.localeCompare(b.textContent);
+    });
+
+    for (let i = 0; i < liArray.length; i++) {
+      ul.appendChild(liArray[i]);
+    }
+  });
 }
